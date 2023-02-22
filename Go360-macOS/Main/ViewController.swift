39d666f43f708160736471c8750eeffed847e062
markpmlim <markpmlim@users.noreply.github.com>
2023-02-22 13:29:48 +0800

    Add files via upload

--- Go360-macOS/Main/ViewController.swift
+++ Go360-macOS/Main/ViewController.swift
@@ -2,11 +2,14 @@
 import Cocoa
 import OpenGL
 
-class ViewController: NSViewController {
+class ViewController: NSViewController
+{
     var oglView: NSOpenGLView {
         return self.view as! NSOpenGLView
     }
-
+    
+    let nameOfVideo = "demo.m4v"
+    
     var currentMouseLocation: CGPoint!
     var previousMouseLocation: CGPoint!
     var rotateX: Float = 0.0
@@ -17,25 +20,35 @@ class ViewController: NSViewController {
     var context: NSOpenGLContext!
     var framesPerSecond = 60.0
     var displayLink: CVDisplayLink?
-
-    override func viewDidLoad() {
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         prepareView()
         setupRenderer()
         setupVideoPlayer()
-
+        
         CVDisplayLinkCreateWithActiveCGDisplays(&displayLink)
         CVDisplayLinkSetOutputCallback(displayLink!,
                                        displayLinkCallback,
                                        UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque()))
         CVDisplayLinkStart(displayLink!)
- 
+        
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(windowWillClose),
                                                name: Notification.Name.NSWindowWillClose,
                                                object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(windowDidMiniaturize),
+                                               name: Notification.Name.NSWindowDidMiniaturize,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(windowDidDeminiaturize),
+                                               name: Notification.Name.NSWindowDidDeminiaturize,
+                                               object: nil)
+        videoPlayer?.play()
     }
-
+    
     let displayLinkCallback: CVDisplayLinkOutputCallback = {
         (displayLink: CVDisplayLink,
         inNow: UnsafePointer<CVTimeStamp>,
@@ -43,7 +56,7 @@ class ViewController: NSViewController {
         flagsIn: CVOptionFlags,
         flagsOut: UnsafeMutablePointer<CVOptionFlags>,
         displayLinkContext: UnsafeMutableRawPointer?) -> CVReturn in
-
+        
         var currentTime = CVTimeStamp()
         CVDisplayLinkGetCurrentTime(displayLink, &currentTime)
         let fps = (currentTime.rateScalar * Double(currentTime.videoTimeScale) / Double(currentTime.videoRefreshPeriod))
@@ -51,10 +64,11 @@ class ViewController: NSViewController {
         vc.updateFrame(fps)
         return kCVReturnSuccess
     }
-
+    
     
     // This method is likely to be called on a secondary thread.
-    @objc func updateFrame(_ framePerSecond: Double) {
+    @objc func updateFrame(_ framePerSecond: Double)
+    {
         renderer?.updateModelViewProjectionMatrix(rotateX, rotateY)
         // Retrieve the video pixel buffer
         guard let pixelBufer = videoPlayer?.retrievePixelBuffer() else {
@@ -69,25 +83,52 @@ class ViewController: NSViewController {
             CGLUnlockContext(self.context.cglContextObj!)
         }
     }
-
-    @objc func windowWillClose(_ notification: Notification) {
+    
+    @objc func windowWillClose(_ notification: Notification)
+    {
         if (notification.object as? NSWindow == self.oglView.window) {
             CVDisplayLinkStop(displayLink!)
         }
     }
-
-    deinit {
-        CVDisplayLinkStop(displayLink!)
+    
+    @objc func windowDidMiniaturize(_ notification: Notification)
+    {
+        if (notification.object as? NSWindow == self.oglView.window) {
+            CVDisplayLinkStop(displayLink!)
+            videoPlayer?.pause()
+        }
     }
-
+    
+    @objc func windowDidDeminiaturize(_ notification: Notification)
+    {
+        if (notification.object as? NSWindow == self.oglView.window) {
+            CVDisplayLinkStart(displayLink!)
+            videoPlayer?.play()
+        }
+    }
+    
+    deinit
+    {
+        CVDisplayLinkStop(displayLink!)
+        NotificationCenter.default.removeObserver(self,
+                                                  name: Notification.Name.NSWindowWillClose,
+                                                  object: nil)
+        NotificationCenter.default.removeObserver(self,
+                                                  name: Notification.Name.NSWindowDidMiniaturize,
+                                                  object: nil)
+        NotificationCenter.default.removeObserver(self,
+                                                  name: Notification.Name.NSWindowDidDeminiaturize,
+                                                  object: nil)
+    }
+    
     override var representedObject: Any? {
         didSet {
             // Update the view, if already loaded.
         }
     }
-
-    private func prepareView() {
-
+    
+    private func prepareView()
+    {
         let pixelFormatAttrsBestCase: [NSOpenGLPixelFormatAttribute] = [
             UInt32(NSOpenGLPFADoubleBuffer),
             UInt32(NSOpenGLPFAAccelerated),
@@ -105,8 +146,9 @@ class ViewController: NSViewController {
         oglView.pixelFormat = pf
         context = oglView.openGLContext!
     }
-
-    private func setupRenderer() {
+    
+    private func setupRenderer()
+    {
         context.makeCurrentContext()
         let shader = Shader()
         let model = Sphere()
@@ -115,18 +157,21 @@ class ViewController: NSViewController {
                             shader: shader,
                             model: model)
     }
-
-    private func setupVideoPlayer() {
-        if let path = Bundle.main.path(forResource: "demo",
-                                       ofType: "m4v") {
+    
+    private func setupVideoPlayer()
+    {
+        let pathComponents = nameOfVideo.components(separatedBy: ".")
+        if let path = Bundle.main.path(forResource: pathComponents[0],
+                                       ofType: pathComponents[1]) {
             let url = URL(fileURLWithPath: path)
-            videoPlayer = VideoPlayer(url: url, framesPerSecond: Int(framesPerSecond))
-            videoPlayer?.play()
+            videoPlayer = VideoPlayer(url: url,
+                                      framesPerSecond: 60)
         }
     }
-
+    
     // This method is called whenever there is a window/view resize
-    override func viewDidLayout() {
+    override func viewDidLayout()
+    {
         let viewSizePoints = oglView.bounds.size
         let viewSizePixels = oglView.convertToBacking(viewSizePoints)
         
@@ -136,39 +181,45 @@ class ViewController: NSViewController {
             CVDisplayLinkStart(displayLink!)
         }
     }
-
-    override func viewWillDisappear() {
+    
+    override func viewWillDisappear()
+    {
         CVDisplayLinkStop(displayLink!)
     }
-
-    override func viewDidAppear() {
+    
+    override func viewDidAppear()
+    {
         self.oglView.window!.makeFirstResponder(self)
     }
-
-    override func keyDown(with event: NSEvent) {
+    
+    override func keyDown(with event: NSEvent)
+    {
         
     }
-
-    override func mouseDown(with event: NSEvent) {
+    
+    override func mouseDown(with event: NSEvent)
+    {
         let mouseLocation = self.view.convert(event.locationInWindow, from: nil)
         currentMouseLocation = mouseLocation
         previousMouseLocation = mouseLocation
     }
-
-    override func mouseDragged(with event: NSEvent) {
+    
+    override func mouseDragged(with event: NSEvent)
+    {
         let mouseLocation = self.view.convert(event.locationInWindow,
                                               from: nil)
-        let radiansPerPoint: Float = 0.001
+        let radiansPerPoint: Float = 0.0001
         var diffX = Float(mouseLocation.x - previousMouseLocation.x)
         var diffY = Float(mouseLocation.y - previousMouseLocation.y)
-
+        
         diffX *= -radiansPerPoint
         diffY *= -radiansPerPoint
         rotateX += diffY
         rotateY += diffX
     }
-
-    override func mouseUp(with event: NSEvent) {
+    
+    override func mouseUp(with event: NSEvent)
+    {
         let mouseLocation = self.view.convert(event.locationInWindow, from: nil)
         previousMouseLocation = mouseLocation
         currentMouseLocation = mouseLocation
